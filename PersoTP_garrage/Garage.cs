25dc61c314114724ca3bf3daff8ec3485aaa2439
Jérémy Sabadie <sabadie.jeremy@gmail.comgit config --global user.name Jeremy>
2023-01-21 18:57:45 +0100

    -Removal of duplicates attributs. -Correction of braces.

--- PersoTP_garrage/Garage.cs
+++ PersoTP_garrage/Garage.cs
@@ -1,14 +1,13 @@
-﻿namespace PersoTP_garrage
-{
-    internal class Garage : Entreprise)
-    {
-        Garage(string name, int siretNumber) : base(name, siretNumber)
-        {
-            string Name = name;
-            int SiretNumber = siretNumber;
-        }
-        private List<Véhicule> Véhicules { get; set; }
-        private List<Garagist> garagist { get; set; }
+﻿namespace PersoTP_garrage;
 
+internal class Garage : Entreprise
+{
+    Garage(string name, int siretNumber) : base(name, siretNumber)
+    {
+        string Name = name;
+        int SiretNumber = siretNumber;
     }
+    private List<Véhicule> Véhicules { get; set; }
+    private List<Garagist> garagist { get; set; }
+
 }
--- PersoTP_garrage/Person.cs
+++ PersoTP_garrage/Person.cs
@@ -4,9 +4,7 @@ internal class Person
 {//Constructeur:
     public Person(string name, string firstName, int age)
     {
-        Name = name;
-        FirstName = firstName;
-        Age = age;
+
     }
     //Attributs:
     private string Name { get; set; }
